d1df1b9e191aaf6e33a46feb0c5e3dcb68efab45
diniavdv <diniavdv@gmail.com>
2021-03-22 10:03:46 +0200

    get user settings

--- plugins/Tracking/TrackingObject.php
+++ plugins/Tracking/TrackingObject.php
@@ -2,5 +2,13 @@
 
 class TrackingObject extends DataAccessObject
 {
-
+    public const TABLE_USER_SETTINGS = 'tracking_user_settings';
+    public const TABLE_PLATFORMS     = 'tracking_platforms';
+    
+    public function searchSettings(array $search)
+    {
+        $sql = "SELECT * FROM ".static::TABLE_USER_SETTINGS;
+    
+        return $this->select($sql, $search);
+    }
 }
