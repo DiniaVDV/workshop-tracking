c23fd67c5bdc0b0183eedeaa48d8b891199541d6
diniavdv <diniavdv@gmail.com>
2021-03-29 14:09:15 +0300

    get commits

--- plugins/Tracking/providers/GitlabProvider.php
+++ plugins/Tracking/providers/GitlabProvider.php
@@ -2,26 +2,26 @@
 
 class GitlabProvider extends AbstractProvider
 {
-    public function loadRemoteData(ValuesObject $valuesObject): array
+    public function loadRemoteData(): array
     {
-        $remoteUserCode = $valuesObject->getRemoveUserCode();
+        $remoteUserCode = $this->getSettingVO()->getRemoveUserCode();
         
         if (!$remoteUserCode) {
-            $remoteUserCode = $this->_getRemoteUserCode($valuesObject);
-            $valuesObject->setRemoteUserCode($remoteUserCode);
+            $remoteUserCode = $this->_getRemoteUserCode();
+            $this->getSettingVO()->setRemoteUserCode($remoteUserCode);
         }
     
-        return $this->_getCommits($valuesObject);
+        return $this->_getCommits();
     }
     
-    private function _getCommits(ValuesObject $valuesObject): array
+    private function _getCommits(): array
     {
-        $projects = $this->_getUserProjects($valuesObject);
+        $projects = $this->_getUserProjects();
     
         $commits = array();
     
         foreach ($projects as $project) {
-            $values = $this->_getProjectCommits($valuesObject, $project);
+            $values = $this->_getProjectCommits($project);
             if ($values) {
                 $commits[] = $values;
             }
@@ -29,32 +29,41 @@ class GitlabProvider extends AbstractProvider
     
         $commits = array_merge(...$commits);
         
-        return $this->_getPreparedCommits($valuesObject, $commits);
-    }
-
-    private function _getPreparedCommits(ValuesObject &$valuesObject, array $commits): array
-    {
-        $data = array();
-        
-        foreach ($commits as $key => $commit) {
-            if ($commit['author_email'] !== $valuesObject->getEmail()) {
-                unset($commits[$key]);
-                continue;
-            }
-    
-            $data[] = new GitlabValuesObject($commit);
-        }
-        
-        return $data;
+        return $commits;
     }
     
-    private function _getRemoteUserCode(ValuesObject &$valuesObject)
+    private function _getCommit(array $project, array $commit): ?array
     {
-        $options = $this->_getDefaultCurlOptions($valuesObject);
+        $options = $this->_getDefaultCurlOptions();
     
         $curl = new Curl($options);
     
-        $url = $valuesObject->getUrl().'user';
+        $url = Core::getInstance()->getUrl(
+            $this->getSettingVO()->getUrl().'projects/%s/repository/commits/%s',
+            $project['id'],
+            $commit['id']
+        );
+    
+    
+        $result = $curl->getUrl($url, false, false, false, true);
+    
+        $response = json_decode($result, true);
+    
+        if (!$response || !is_array($response) || !array_key_exists('id', $response)) {
+            //TODO Think
+            return null;
+        }
+    
+        return $response;
+    }
+    
+    private function _getRemoteUserCode()
+    {
+        $options = $this->_getDefaultCurlOptions();
+    
+        $curl = new Curl($options);
+    
+        $url = $this->getSettingVO()->getUrl().'user';
         
         $result = $curl->getUrl($url, false, false, false, true);
         
@@ -67,19 +76,19 @@ class GitlabProvider extends AbstractProvider
         
         $remoteCode = $response['id'];
         
-        return $response['id'];
+        return $remoteCode;
     }
     
-    private function _getUserProjects(ValuesObject $valuesObject)
+    private function _getUserProjects()
     {
-        $remoteUserCode = $valuesObject->getRemoveUserCode();
+        $remoteUserCode = $this->getSettingVO()->getRemoveUserCode();
         
         $url = Core::getInstance()->getUrl(
-            $valuesObject->getUrl().'users/%s/projects',
+            $this->getSettingVO()->getUrl().'users/%s/projects',
             $remoteUserCode
         );
         
-        $options = $this->_getDefaultCurlOptions($valuesObject);
+        $options = $this->_getDefaultCurlOptions();
     
         $curl = new Curl($options);
     
@@ -95,34 +104,53 @@ class GitlabProvider extends AbstractProvider
         return $response;
     }
     
-    private function _getProjectCommits(ValuesObject $valuesObject, $project): array
+    private function _getProjectCommits(array $project): ?array
     {
         $url = Core::getInstance()->getUrl(
-            $valuesObject->getUrl().'projects/%s/repository/commits',
+            $this->getSettingVO()->getUrl().'projects/%s/repository/commits',
             $project['id']
         );
     
-        $options = $this->_getDefaultCurlOptions($valuesObject);
+        $options = $this->_getDefaultCurlOptions();
     
         $curl = new Curl($options);
     
         $result = $curl->getUrl($url, false, false, false, true);
     
-        $response = json_decode($result, true);
+        $commits = json_decode($result, true);
     
-        if (!$response || !is_array($response) || !is_array(current($response))) {
+        if (!$commits || !is_array($commits) || !is_array(current($commits))) {
             //TODO Think
             return null;
         }
     
-        return $response;
+        return $this->_getCommitsWithAdditionalData($project, $commits);
     }
     
-    private function _getDefaultCurlOptions(ValuesObject $valuesObject): array
+    private function _getCommitsWithAdditionalData(array $project, array $commits): array
+    {
+        $data = array();
+        
+        foreach ($commits as $commit) {
+            if ($commit['author_email'] !== $this->getSettingVO()->getEmail()) {
+                continue;
+            }
+    
+            $commitData = $this->_getCommit($project, $commit);
+    
+            if ($commitData) {
+                $data[] = new GitlabValuesObject($commitData);
+            }
+        }
+        
+        return $data;
+    }
+    
+    private function _getDefaultCurlOptions(): array
     {
         return array(
             CURLOPT_HTTPHEADER => array(
-                'Authorization: Bearer '.$valuesObject->getRemoteApiToken(),
+                'Authorization: Bearer '.$this->getSettingVO()->getRemoteApiToken(),
             ),
             CURLOPT_FAILONERROR => 0,
         );
