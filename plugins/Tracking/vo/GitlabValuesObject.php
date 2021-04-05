85af1af3fa17bee9cf73e350ece18c98683df9d3
diniavdv <diniavdv@gmail.com>
2021-04-05 11:29:20 +0300

    prepare values for create

--- plugins/Tracking/vo/GitlabValuesObject.php
+++ plugins/Tracking/vo/GitlabValuesObject.php
@@ -1,11 +1,57 @@
 <?php
 
-class GitlabValuesObject extends ValuesObject
+class GitlabValuesObject extends ValuesObject implements ITrackingValuesObject
 {
+    public function getID(): string
+    {
+        return $this->get('id');
+    }
+    
+    public function getHash(): string
+    {
+        return $this->get('hash');
+    }
+    
+    public function getMessage(): string
+    {
+        return $this->get('message');
+    }
+    
+    public function getDate(): string
+    {
+        return $this->get('created_at');
+    }
+    
+    public function getProjectName(): string
+    {
+        return $this->get('project_name');
+    }
+    
+    public function getProjectID(): string
+    {
+        return $this->get('project_id');
+    }
+    
+    public function getWebUrl(): string
+    {
+        return $this->get('url');
+    }
+    
+    public function getUrlToProject(): string
+    {
+        return $this->get('url_to_project');
+    }
+    
     public function getCreateValues(): array
     {
         return array(
-        
+            'hash'           => $this->getHash(),
+            'message'        => $this->getMessage(),
+            'date'           => $this->getDate(),
+            'project_name'   => $this->getProjectName(),
+            'project_id'     => $this->getProjectID(),
+            'url'            => $this->getUrl(),
+            'url_to_project' => $this->getUrlToProject(),
         );
     }
 }
--- plugins/Tracking/vo/RedmineValuesObject.php
+++ plugins/Tracking/vo/RedmineValuesObject.php
@@ -1,12 +1,70 @@
 <?php
 
 
-class RedmineValuesObject extends ValuesObject
+class RedmineValuesObject extends ValuesObject implements ITrackingValuesObject
 {
+    public function getID(): int
+    {
+        return $this->get('id');
+    }
+    
+    public function getStartDate(): string
+    {
+        return $this->get('start_date');
+    }
+    
+    public function getSpentOn(): string
+    {
+        return $this->get('spent_on');
+    }
+    
+    public function getProjectName(): string
+    {
+        return $this->get('project_name');
+    }
+    
+    public function getProjectID(): string
+    {
+        return $this->get('project_id');
+    }
+    
+    public function getPriorityName(): string
+    {
+        return $this->get('priority_name');
+    }
+    
+    public function getUrl(): string
+    {
+        return $this->get('url');
+    }
+    
+    public function getCommets(): string
+    {
+        return $this->get('comments');
+    }
+    
+    public function getHour(): string
+    {
+        return $this->get('hours');
+    }
+    
+    public function getSubject(): string
+    {
+        return $this->get('subject');
+    }
+    
     public function getCreateValues(): array
     {
         return array(
-        
+            'subject'       => $this->getSubject(),
+            'hours'         => $this->getHour(),
+            'comments'      => $this->getCommets(),
+            'start_date'    => $this->getStartDate(),
+            'spent_on'      => $this->getSpentOn(),
+            'project_name'  => $this->getProjectName(),
+            'project_id'    => $this->getProjectID(),
+            'priority_name' => $this->getPriorityName(),
+            'url'           => $this->getUrl(),
         );
     }
 }
